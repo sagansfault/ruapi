009ba06a995052b7e4b4fe0f3b632a1984a7cb8f
sagan <camjanssen120@gmail.com>
2023-12-17 15:50:24 -0800

    add proper character names

--- src/character.rs
+++ src/character.rs
@@ -2,32 +2,32 @@ use std::hash::{Hash, Hasher};
 use once_cell::sync::Lazy;
 use regex::Regex;
 
-pub static SOL: Lazy<Character> = Lazy::new(|| Character::new("SO", 0, r"(?i)(sol)"));
-pub static KY: Lazy<Character> = Lazy::new(|| Character::new("KY", 1, r"(?i)(ky)"));
-pub static MAY: Lazy<Character> = Lazy::new(|| Character::new("MA", 2, r"(?i)(may)"));
-pub static AXL: Lazy<Character> = Lazy::new(|| Character::new("AX", 3, r"(?i)(axl)"));
-pub static CHIPP: Lazy<Character> = Lazy::new(|| Character::new("CH", 4, r"(?i)(chip)"));
-pub static POTEMKIN: Lazy<Character> = Lazy::new(|| Character::new("PO", 5, r"(?i)(pot)"));
-pub static FAUST: Lazy<Character> = Lazy::new(|| Character::new("FA", 6, r"(?i)(faust)"));
-pub static MILLIA: Lazy<Character> = Lazy::new(|| Character::new("MI", 7, r"(?i)(mill?ia)"));
-pub static ZATO: Lazy<Character> = Lazy::new(|| Character::new("ZA", 8, r"(?i)(zato)"));
-pub static RAMLETHAL: Lazy<Character> = Lazy::new(|| Character::new("RA", 9, r"(?i)(ram)"));
-pub static LEO: Lazy<Character> = Lazy::new(|| Character::new("LE", 10, r"(?i)(leo)"));
-pub static NAGORIYUKI: Lazy<Character> = Lazy::new(|| Character::new("NA", 11, r"(?i)(nago)"));
-pub static GIOVANNA: Lazy<Character> = Lazy::new(|| Character::new("GI", 12, r"(?i)(gio)"));
-pub static ANJI: Lazy<Character> = Lazy::new(|| Character::new("AN", 13, r"(?i)(anji)"));
-pub static INO: Lazy<Character> = Lazy::new(|| Character::new("IN", 14, r"(?i)(i-?no)"));
-pub static GOLDLEWIS: Lazy<Character> = Lazy::new(|| Character::new("GO", 15, r"(?i)(gl|gold|lewis|go)"));
-pub static JACKO: Lazy<Character> = Lazy::new(|| Character::new("JC", 16, r"(?i)(jack|jc)"));
-pub static HAPPYCHAOS: Lazy<Character> = Lazy::new(|| Character::new("HA", 17, r"(?i)(ha|hc|happy|chaos)"));
-pub static BAIKEN: Lazy<Character> = Lazy::new(|| Character::new("BA", 18, r"(?i)(baiken)"));
-pub static TESTAMENT: Lazy<Character> = Lazy::new(|| Character::new("TE", 19, r"(?i)(test)"));
-pub static BRIDGET: Lazy<Character> = Lazy::new(|| Character::new("BI", 20, r"(?i)(bridget)"));
-pub static SIN: Lazy<Character> = Lazy::new(|| Character::new("SI", 21, r"(?i)(sin)"));
-pub static BEDMAN: Lazy<Character> = Lazy::new(|| Character::new("BE", 22, r"(?i)(bedman)"));
-pub static ASUKA: Lazy<Character> = Lazy::new(|| Character::new("AS", 23, r"(?i)(asuka)"));
-pub static JOHNNY: Lazy<Character> = Lazy::new(|| Character::new("JN", 24, r"(?i)(johnn?y)"));
-pub static ELPHELT: Lazy<Character> = Lazy::new(|| Character::new("EL", 25, r"(?i)(elph)"));
+pub static SOL: Lazy<Character> = Lazy::new(|| Character::new("SO", "Sol", 0, r"(?i)(sol)"));
+pub static KY: Lazy<Character> = Lazy::new(|| Character::new("KY", "Ky", 1, r"(?i)(ky)"));
+pub static MAY: Lazy<Character> = Lazy::new(|| Character::new("MA", "May", 2, r"(?i)(may)"));
+pub static AXL: Lazy<Character> = Lazy::new(|| Character::new("AX", "Axl", 3, r"(?i)(axl)"));
+pub static CHIPP: Lazy<Character> = Lazy::new(|| Character::new("CH", "Chipp", 4, r"(?i)(chip)"));
+pub static POTEMKIN: Lazy<Character> = Lazy::new(|| Character::new("PO", "Potemkin", 5, r"(?i)(pot)"));
+pub static FAUST: Lazy<Character> = Lazy::new(|| Character::new("FA", "Faust", 6, r"(?i)(faust)"));
+pub static MILLIA: Lazy<Character> = Lazy::new(|| Character::new("MI", "Millia", 7, r"(?i)(mill?ia)"));
+pub static ZATO: Lazy<Character> = Lazy::new(|| Character::new("ZA", "Zato-1", 8, r"(?i)(zato)"));
+pub static RAMLETHAL: Lazy<Character> = Lazy::new(|| Character::new("RA", "Ramlethal", 9, r"(?i)(ram)"));
+pub static LEO: Lazy<Character> = Lazy::new(|| Character::new("LE", "Leo", 10, r"(?i)(leo)"));
+pub static NAGORIYUKI: Lazy<Character> = Lazy::new(|| Character::new("NA", "Nagoriyuki", 11, r"(?i)(nago)"));
+pub static GIOVANNA: Lazy<Character> = Lazy::new(|| Character::new("GI", "Giovanna", 12, r"(?i)(gio)"));
+pub static ANJI: Lazy<Character> = Lazy::new(|| Character::new("AN", "Anji", 13, r"(?i)(anji)"));
+pub static INO: Lazy<Character> = Lazy::new(|| Character::new("IN", "I-No", 14, r"(?i)(i-?no)"));
+pub static GOLDLEWIS: Lazy<Character> = Lazy::new(|| Character::new("GO", "Goldlewis", 15, r"(?i)(gl|gold|lewis|go)"));
+pub static JACKO: Lazy<Character> = Lazy::new(|| Character::new("JC", "Jacko", 16, r"(?i)(jack|jc)"));
+pub static HAPPYCHAOS: Lazy<Character> = Lazy::new(|| Character::new("HA", "Happy Chaos", 17, r"(?i)(ha|hc|happy|chaos)"));
+pub static BAIKEN: Lazy<Character> = Lazy::new(|| Character::new("BA", "Baiken", 18, r"(?i)(baiken)"));
+pub static TESTAMENT: Lazy<Character> = Lazy::new(|| Character::new("TE", "Testamnet", 19, r"(?i)(test)"));
+pub static BRIDGET: Lazy<Character> = Lazy::new(|| Character::new("BI", "Bridget", 20, r"(?i)(bridget)"));
+pub static SIN: Lazy<Character> = Lazy::new(|| Character::new("SI", "Sin", 21, r"(?i)(sin)"));
+pub static BEDMAN: Lazy<Character> = Lazy::new(|| Character::new("BE", "Bedman", 22, r"(?i)(bedman)"));
+pub static ASUKA: Lazy<Character> = Lazy::new(|| Character::new("AS", "Asuka", 23, r"(?i)(asuka)"));
+pub static JOHNNY: Lazy<Character> = Lazy::new(|| Character::new("JN", "Johnny", 24, r"(?i)(johnn?y)"));
+pub static ELPHELT: Lazy<Character> = Lazy::new(|| Character::new("EL", "Elphelt", 25, r"(?i)(elph)"));
 
 pub static CHARACTERS: Lazy<[&Character; 26]> = Lazy::new(|| [
     &SOL, &KY, &AXL, &MAY, &CHIPP, &POTEMKIN, &FAUST, &MILLIA, &ZATO, &RAMLETHAL, &LEO,
@@ -50,6 +50,7 @@ pub fn get_character_id<'a>(id: usize) -> Option<&'a Character> {
 #[derive(Clone, Debug)]
 pub struct Character {
     pub shortname: String,
+    pub readablename: String,
     pub id: usize,
     pub matcher: Regex,
 }
@@ -69,9 +70,12 @@ impl Hash for Character {
 impl Eq for Character {}
 
 impl Character {
-    fn new(short: &str, id: usize, matcher: &str) -> Self {
+    fn new(short: &str, readable: &str, id: usize, matcher: &str) -> Self {
         Character {
-            shortname: short.to_string(), id, matcher: Regex::new(&format!(r"(?i)({})", matcher)).unwrap(),
+            shortname: short.to_string(),
+            readablename: readable.to_string(),
+            id,
+            matcher: Regex::new(&format!(r"(?i)({})", matcher)).unwrap(),
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,22 @@ async fn test() {
     let match_history = rating::load_match_history_id(&player_data.id, &character::GOLDLEWIS).await.unwrap();
     println!("Match history test: {:?}", match_history.first().unwrap());
 
-    let matchup = matchup::load_matchups(matchup::MatchupChart::TopHundred).await.unwrap();
-    println!("Matchup test: GO vs PO = {}", matchup.get_matchup(&character::GOLDLEWIS, &character::POTEMKIN).unwrap());
+    let matchups = matchup::load_matchups(matchup::MatchupChart::TopHundred).await.unwrap();
+    println!("Matchup test: GO vs PO = {}", matchups.get_matchup(&character::GOLDLEWIS, &character::POTEMKIN).unwrap());
+
+    let mut tierlist: Vec<(String, f64)> = vec![];
+    for (character, mus) in matchups.matchups {
+        let count = mus.len() as f64;
+        let sum = mus.values().sum::<f64>();
+        let avg_winrate = sum / count;
+        tierlist.push((character.readablename.clone(), avg_winrate));
+    }
+    tierlist.sort_by(|(_, b), (_, d)| b.partial_cmp(d).unwrap());
+    tierlist.reverse();
+    let tierlist = tierlist.into_iter()
+        .enumerate()
+        .map(|(ind, (c, f))| format!("{}. {} ({:.1}%)", ind + 1, c, f))
+        .collect::<Vec<String>>()
+        .join("\n");
+    println!("Tierlist from average winrates:\n{}", tierlist);
 }
